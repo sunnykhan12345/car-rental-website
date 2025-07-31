21f4c8798520d709493663d862e21cba841862fd
sana ullah <sunnykhansadda@gmail.com>
2025-07-31 19:45:01 +0500

    done

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import { Filter, Search } from "lucide-react";
+import React from "react";
 
-const Title = ({title,subTitle,align}) => {
+const Title = ({ title, subTitle, align }) => {
   return (
-    <div className={`flex flex-col justify-center items-center text-center ${align === "left" && "md:items-start md:text-left"}`}>
-     <h1 className='font-semibold text-gray-700 text-2xl pb-3'>{title}</h1>
-     <p className='text-base font-normal'>{subTitle}</p>
+    <div
+      className={`flex flex-col justify-center items-center text-center ${
+        align === "left" && "md:items-start md:text-left"
+      }`}
+    >
+      <h1 className="font-semibold text-gray-700 text-2xl pb-3">{title}</h1>
+      <p className="text-base font-normal">{subTitle}</p>
+      <div className="bg-white flex items-center px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow cursor-pointer">
+        <Search className="w-5 h-5 " />
+        <input
+          type="text"
+          placeholder="Search by make,Model or feature"
+          className="w-full focus:outline-0 px-3 text-gray-500"
+        />
+        <Filter className="w-5 h-5 " />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Title from "../components/Title"
 const Cars = () => {
   return (
-    <div>Cars</div>
+    <div className='flex flex-col items-center py-20 bg-light max-md:px-4'>
+      <Title title="Available cars" subTitle="Browse your selection of premisum vehicles avialable for your next advanture"/>
+    </div>
   )
 }
 
