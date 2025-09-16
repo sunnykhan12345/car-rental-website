0d6003c587e5dd013ac686bdba83687831c37022
sana ullah <sunnykhansadda@gmail.com>
2025-09-17 00:51:43 +0500

    working on conect api

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,3 @@
-// import React, { useState } from "react";
-// import Navbar from "./components/Navbar";
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Cars from "./pages/Cars";
-// import CarsDetails from "./pages/CarsDetails";
-// import MyBooking from "./pages/MyBooking";
-// import { Footer } from "./components/Footer";
-// import Layout from "./pages/owner/Layout"
-// import Addcart from "./pages/owner/AddCart";
-// const App = () => {
-//   const [showLogin, setShowLogin] = useState(false);
-//   return (
-//     <>
-//       <Navbar setShowLogin={setShowLogin} />
-//       {showLogin && <div>Login Modal Goes Here</div>}
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/car-details/:id" element={<CarsDetails />} />
-//         <Route path="/cars" element={<Cars />} />
-//         <Route path="/my-bookings" element={<MyBooking />} />
-//         <Route path="/owner" element={<Layout />}>
-//           <Route index element={<Dashbaord />} />
-//           <Route index element={<Addcart />} />
-//         </Route>
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default App;
-
-
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -41,16 +7,17 @@ import MyBooking from "./pages/MyBooking";
 import { Footer } from "./components/Footer";
 import Layout from "./pages/owner/Layout";
 import Addcart from "./pages/owner/AddCart";
-import Dashboard from "./pages/owner/Dashbaord"; 
-import ManageCar from "./pages/owner/ManageCar"; 
-import ManageBookings from "./pages/owner/ManageBookings"; 
+import Dashboard from "./pages/owner/Dashbaord";
+import ManageCar from "./pages/owner/ManageCar";
+import ManageBookings from "./pages/owner/ManageBookings";
 import { useState } from "react";
-
+import { Toaster } from "react-hot-toast";
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
+      <Toaster />
       <Navbar setShowLogin={setShowLogin} />
       {showLogin && <div>Login Modal Goes Here</div>}
 
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,95 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+const currency = import.meta.env.VITE_CURRENCY;
+export const AppContext = createContext();
+
+export const AppProivder = ({ children }) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwener] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickUpDate, setPickUpDate] = useState("");
+  const [returnDate, setRuternDate] = useState("");
+  const [cars, setcCars] = useState([]);
+
+  // function to chec is user logedin
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.fetch("/api/user/data");
+      if (data.success) {
+        setUser(data.user);
+        setIsOwener(data.user.role === "owner");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // function to fetch all cars from the server
+  const fetchCars = async () => {
+    try {
+      const { data } = await axios.fetch("/api/user/cars");
+      data.success ? setcCars(data.cars) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // useeffect to retrive the token from localhost
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+    setcCars();
+  }, []);
+
+  // function to logout the user
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+    setIsOwener(false);
+    axios.defaults.headers.common["Authorization"] = "";
+    toast.success("You Have been logged out");
+  };
+  // useeffect to fecth user data when user is avialabel
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `${token}`;
+      fetchUser();
+    }
+  }, [token]);
+  const value = {
+    navigate,
+    currency,
+    axios,
+    user,
+    setUser,
+    token,
+    setToken,
+    isOwner,
+    setIsOwener,
+    fetchUser,
+    showLogin,
+    setShowLogin,
+    logOut,
+    fetchCars,
+    cars,
+    setcCars,
+    pickUpDate,
+    returnDate,
+    setPickUpDate,
+    setRuternDate,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
